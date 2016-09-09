8ff007aa9e5bee2b119e11b312e6f4d8c2fce2ed
Tomasz Sikora <frakti7@gmail.com>
2016-09-09 09:19:07 +0200

    replace reverse with snake endpoint

--- routes/command.js
+++ routes/command.js
@@ -3,14 +3,14 @@
 const _ = require('lodash')
 
 module.exports = (app) => {
-  app.get('/reverse', (req, res) => {
+  app.get('/snake', (req, res) => {
     if (req.query.token !== 'zRWY8EfErKj8vIYiR5Nl9i91') {
       return res.sendStatus(409)
     }
 
     res.json({
       response_type: 'in_channel',
-      text: _.reverse(req.query.text)
+      text: _.snakeCase(req.query.text)
     })
   })
 }
