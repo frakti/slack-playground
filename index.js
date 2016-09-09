75fec74887bd618db8481e1dda90ddfca1b78364
Tomasz Sikora <frakti7@gmail.com>
2016-09-09 09:56:09 +0200

    fix

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
-const bot = require('./bot')
+require('./bot')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.json())
 
 routes(app)
-bot()
 
 app.listen(app.get('port'), () => console.log('Node app is running on port', app.get('port')))
