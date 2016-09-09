3b7f7b208cc87903c0a80d9dd6a66223c0596619
Tomasz Sikora <frakti7@gmail.com>
2016-09-09 09:49:06 +0200

    add bot

---
+++ routes/botkit.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Botkit = require('botkit')
+const controller = Botkit.slackbot()
+const bot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN
+})
+
+bot.startRTM((err, bot, payload) => {
+  if (err) {
+    throw new Error('Could not connect to Slack')
+  }
+})
+
+controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
+    bot.api.reactions.add({
+        timestamp: message.ts,
+        channel: message.channel,
+        name: 'robot_face',
+    }, function(err, res) {
+        if (err) {
+            bot.botkit.log('Failed to add emoji reaction :(', err);
+        }
+    });
+
+
+    controller.storage.users.get(message.user, function(err, user) {
+        if (user && user.name) {
+            bot.reply(message, 'Hello ' + user.name + '!!');
+        } else {
+            bot.reply(message, 'Hello.');
+        }
+    });
+});
