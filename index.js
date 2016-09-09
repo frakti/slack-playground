e020e53537f96ca083160d91962503b1e0e3ac28
Tomasz Sikora <frakti7@gmail.com>
2016-09-09 08:51:46 +0200

    support json body

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+'use strict'
+
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.json({ok: 'test'})
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'))
-})
+app.listen(app.get('port'), () => console.log('Node app is running on port', app.get('port')))
