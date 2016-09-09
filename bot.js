71aae3ebbbb31f431def09b6cc0b13589809b60c
Tomasz Sikora <frakti7@gmail.com>
2016-09-09 10:26:24 +0200

    fix formatting

--- bot.js
+++ bot.js
@@ -13,21 +13,21 @@ bot.startRTM((err, bot, payload) => {
 })
 
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', (bot, message) => {
-    bot.api.reactions.add({
-        timestamp: message.ts,
-        channel: message.channel,
-        name: 'robot_face',
-    }, function(err, res) {
-        if (err) {
-            bot.botkit.log('Failed to add emoji reaction :(', err)
-        }
-    })
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  }, function(err, res) {
+    if (err) {
+      bot.botkit.log('Failed to add emoji reaction :(', err)
+    }
+  })
 
-    controller.storage.users.get(message.user, (err, user) => {
-        if (user && user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!')
-        } else {
-            bot.reply(message, 'Hello.')
-        }
-    })
+  controller.storage.users.get(message.user, (err, user) => {
+    if (user && user.name) {
+      bot.reply(message, 'Hello ' + user.name + '!!')
+    } else {
+      bot.reply(message, 'Hello.')
+    }
+  })
 })
