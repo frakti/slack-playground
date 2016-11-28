9ea84d60cd092712d02ecf0a529bad01be5e369a
Tomasz Sikora <frakti7@gmail.com>
2016-11-28 21:10:27 +0100

    add command to calculate exchange ratesa

--- routes/command.js
+++ routes/command.js
@@ -1,16 +1,52 @@
 'use strict'
 
-const _ = require('lodash')
+const fetch = require('node-fetch')
+const fx = require('money')
 
 module.exports = (app) => {
-  app.get('/snake', (req, res) => {
-    if (req.query.token !== process.env.SLACK_SNAKE_COMMAND_TOKEN) {
+  app.get('/exchange', (req, res) => {
+    if (req.query.token !== process.env.SLACK_COMMAND_TOKEN) {
       return res.sendStatus(409)
     }
 
-    res.json({
-      response_type: 'in_channel',
-      text: _.snakeCase(req.query.text)
-    })
+    const pattern = /([0-1.]+) (.+) to (.+)/
+
+    if (!pattern.test(req.query.text)) {
+      return res.json({
+        response_type: 'in_channel',
+        text: 'Wrong pattern, try something like: _10 usd to pln_'
+      })
+    }
+
+    fetch('http://api.fixer.io/latest')
+      .then(response => {
+        if (response.ok) {
+          return res.json({
+            response_type: 'in_channel',
+            text: "Couldn't fetch rates, please try again."
+          })
+        }
+
+        return response.json()
+      })
+      .then(({rates}) => {
+        const [, value, from, to] = pattern.exec(req.query.text)
+        fx.rates = rates
+
+        const text = fx(value)
+          .from(from.toUpperCase())
+          .to(to.toUpperCase())
+
+        return res.json({
+          response_type: 'in_channel',
+          text
+        })
+      })
+      .catch(err => {
+        return res.json({
+          response_type: 'in_channel',
+          text: "Couldn't fetch rates, please try again."
+        })
+      })
   })
 }
